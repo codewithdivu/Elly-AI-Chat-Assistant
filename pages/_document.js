be53d8d1329374e8a3e03228e9f47725259a26f8
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-04-27 23:28:19 +0530

    added some design

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/chat/index.js
+++ pages/chat/index.js
@@ -7,7 +7,7 @@ const ChatPage = () => {
   const [storedValues, setStoredValues] = useState([]);
 
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: process.env.API_KEY,
   });
   delete configuration.baseOptions.headers["User-Agent"];
   const openai = new OpenAIApi(configuration);
@@ -44,22 +44,66 @@ const ChatPage = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
-      <input
-        type="text"
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-      />
-      <button>Submit</button>
+    <div className="container">
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <button className="sidebar-header-btn">
+            <span>+</span> New chat
+          </button>
+        </div>
+
+        <div className="sidebar-main">
+          {Array.apply(null, Array(5)).map((item, index) => (
+            <div key={index} className="sidebar-main-item">
+              <i style={{ marginLeft: "-1rem" }} className="fa fa-message" />
+              <p style={{ marginLeft: "-2.5rem" }}>How are you?</p>
+              <i
+                className="fa fa-edit"
+                style={{
+                  marginRight: "-3rem",
+                }}
+              />
+              <i
+                className="fa fa-trash"
+                style={{
+                  marginRight: "-1.2rem",
+                }}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="sidebar-footer">
+          <div className="sidebar-footer-item">
+            <i style={{ marginLeft: "-1rem" }} className="fa fa-user" />
+            <p style={{ marginLeft: "-2.5rem" }}>Upgrade to Plus</p>
+            <button className="sidebar-footer-btn">New</button>
+          </div>
+          <div className="sidebar-footer-item">
+            <i style={{ marginLeft: "-1.8rem" }} className="fa fa-user" />
+            <p style={{ marginLeft: "-4.5rem" }}>Divyesh Mavadiya</p>
+            <i
+              style={{ marginLeft: "-1rem" }}
+              className="fa fa-ellipsis-vertical"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="searchbar">
+        <div className="searchbar-main">
+          <input
+            className="searchbar-input"
+            type="text"
+            placeholder="Send a Message..."
+            name="search"
+          />
+          <button className="searchbar-btn" type="submit">
+            <i class="fa fa-paper-plane"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="main">Main Container</div>
     </div>
   );
 };
